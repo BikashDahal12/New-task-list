807a4022b4cb2b3d2814249b1c60b9d7be0d646e
BikashDahal12 <bikashdahal824@gmail.com>
2024-10-18 17:25:05 +0545

    task list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import TaskForm from './components/TaskForm';
+
+function App() {
+  return (
+    <div>
+      <TaskForm />
+    </div>
+  );
+}
+
+export default App;
